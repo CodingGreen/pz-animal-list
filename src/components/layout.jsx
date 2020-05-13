c6bf66ff4ab8a2630a973e4260bdd1bb6309b9b4
CodingGreen <17253156+CodingGreen@users.noreply.github.com>
2020-05-13 19:36:26 +0100

    :lipstick: Basic table styling

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -11,7 +11,8 @@ import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './layout.css';
+// import './layout.css';
+import './myStyles.css';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
