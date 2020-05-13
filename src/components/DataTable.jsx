e99a952daa92cc3e9a45b149df8cc48f9ac9e21a
CodingGreen <17253156+CodingGreen@users.noreply.github.com>
2020-05-13 19:40:47 +0100

    :bug: Corrected checkbox id's

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -24,7 +24,7 @@ const DataTable = () => {
                 inline
                 label={column.id}
                 type="checkbox"
-                id="Checkbox"
+                id={`checkbox-${column.id}`}
                 {...column.getToggleHiddenProps()}
               />
             </label>
