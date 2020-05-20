d40d04a051220d3bcda438be9b003ab0130e6e9a
CodingGreen <17253156+CodingGreen@users.noreply.github.com>
2020-05-20 17:28:18 +0100

    :sparkles: Added PWA and offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'Unofficial Planet Zoo Animal List',
+    description: 'This unofficial tool for the game Planet Zoo allows players to sort and filter the animals in the game to find the perfect animals to add to their zoo.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    // {
-    //   resolve: 'gatsby-plugin-manifest',
-    //   options: {
-    //     name: 'gatsby-starter-default',
-    //     short_name: 'starter',
-    //     start_url: '/',
-    //     background_color: '#663399',
-    //     theme_color: '#663399',
-    //     display: 'minimal-ui',
-    //   },
-    // },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Unofficial Planet Zoo Animal List',
+        short_name: 'PZ Animal List',
+        start_url: '/',
+        background_color: '#FFFFFF',
+        theme_color: '#007bff',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.svg',
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 };
